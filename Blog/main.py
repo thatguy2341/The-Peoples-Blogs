32f1a4c2a035987ad7219edc6291500f98ef964f
Guy Newman <gamenewman@gmail.com>
2023-02-14 03:41:47 +0200

    Fix Format

--- Blog/main.py
+++ Blog/main.py
@@ -20,7 +20,7 @@ app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.app_context().push()
 db = SQLAlchemy(app)
