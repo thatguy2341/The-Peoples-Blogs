962317ee51c3a3a147091332cf450170df454d92
Guy Newman <gamenewman@gmail.com>
2024-01-14 01:26:33 +0200

    remove logging

--- Blog/static/cleanJS/searchSystem3.0/blogsController.js
+++ Blog/static/cleanJS/searchSystem3.0/blogsController.js
@@ -34,11 +34,6 @@ const startBlogs = async function () {
 };
 
 const controlGetPage = function () {
-  console.log(
-    (paginationView.page - 1) * AMOUNT_OF_PAGES_PER_BLOG,
-    (paginationView.page - 1) * AMOUNT_OF_PAGES_PER_BLOG +
-      AMOUNT_OF_PAGES_PER_BLOG
-  );
   blogsView.renderSpinner();
   blogsView.showBlogs(
     state.blogs.slice(
